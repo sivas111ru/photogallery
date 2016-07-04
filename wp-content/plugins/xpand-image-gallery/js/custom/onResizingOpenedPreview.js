551c0989baa39f790310d9aeeaeb2973c327ac63
Vlad Pereskokov <pereskokow@gmail.com>
2016-07-04 20:49:44 +0300

    fix: if preview is open and then hidden then height is reset on the image

--- wp-content/plugins/xpand-image-gallery/js/custom/onResizingOpenedPreview.js
+++ wp-content/plugins/xpand-image-gallery/js/custom/onResizingOpenedPreview.js
@@ -5,7 +5,14 @@ var MOBILE_SCREEN_RESOLUTION = 600;
 jQuery(document).ready(function ($) {
   $(window).on("resize", function () {
     if ( window.IS_PREVIEW_SHOWN && window.innerWidth < MOBILE_SCREEN_RESOLUTION ) {
+      var $prev_ul = $(".gallery-item.og-expanded");
+
       hidePreview();
+
+      setTimeout(function(){
+        $prev_ul.css("height", "");
+      }, 10);
+
       window.IS_PREVIEW_SHOWN = false;
     }
   });
